3239059d0d0b72bb8fdbcf204e27d6ee9635b160
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-03-06 21:44:09 +0100

    Publish value

--- components/soyosource_virtual_meter/number/soyosource_number.cpp
+++ components/soyosource_virtual_meter/number/soyosource_number.cpp
@@ -8,7 +8,7 @@ static const char *const TAG = "soyosource_virtual_meter.number";
 
 void SoyosourceNumber::control(float value) {
   this->parent_->set_manual_power_demand((uint16_t) value);
-  this->publish_state(state);
+  this->publish_state(value);
 }
 void SoyosourceNumber::dump_config() { LOG_NUMBER(TAG, "SoyosourceVirtualMeter Number", this); }
 
