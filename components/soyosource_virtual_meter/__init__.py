3dd0fe443e2d4791a26613319d272a5209f6660d
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-03-06 21:45:04 +0100

    Introduce new calculate power demand method

--- components/soyosource_virtual_meter/__init__.py
+++ components/soyosource_virtual_meter/__init__.py
@@ -9,6 +9,8 @@ MULTI_CONF = True
 
 CONF_SOYOSOURCE_VIRTUAL_METER_ID = "soyosource_virtual_meter_id"
 CONF_POWER_ID = "power_id"
+CONF_POWER_SENSOR_INACTIVITY_TIMEOUT = "power_sensor_inactivity_timeout"
+CONF_POWER_DEMAND_CALCULATION = "power_demand_calculation"
 CONF_MIN_POWER_DEMAND = "min_power_demand"
 CONF_MAX_POWER_DEMAND = "max_power_demand"
 CONF_BUFFER = "buffer"
@@ -23,6 +25,12 @@ SoyosourceVirtualMeter = soyosource_virtual_meter_ns.class_(
     cg.PollingComponent,
     soyosource_modbus.SoyosourceModbusDevice,
 )
+
+PowerDemandCalculation = soyosource_virtual_meter_ns.enum("PowerDemandCalculation")
+POWER_DEMAND_CALCULATION_OPTIONS = {
+    "DUMB_OEM_BEHAVIOR": PowerDemandCalculation.POWER_DEMAND_CALCULATION_DUMB_OEM_BEHAVIOR,
+    "NEGATIVE_MEASUREMENTS_REQUIRED": PowerDemandCalculation.POWER_DEMAND_CALCULATION_NEGATIVE_MEASUREMENTS_REQUIRED,
+}
 
 
 def validate_min_max(config):
@@ -40,7 +48,13 @@ CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(SoyosourceVirtualMeter),
+            cv.Optional(
+                CONF_POWER_DEMAND_CALCULATION, default="DUMB_OEM_BEHAVIOR"
+            ): cv.enum(POWER_DEMAND_CALCULATION_OPTIONS, upper=True),
             cv.Required(CONF_POWER_ID): cv.use_id(sensor.Sensor),
+            cv.Optional(
+                CONF_POWER_SENSOR_INACTIVITY_TIMEOUT, default="20s"
+            ): cv.positive_time_period_seconds,
             cv.Optional(CONF_BUFFER, default=DEFAULT_BUFFER): cv.int_range(
                 min=-200, max=200
             ),
@@ -64,7 +78,14 @@ async def to_code(config):
     await soyosource_modbus.register_soyosource_modbus_device(var, config)
 
     sens = await cg.get_variable(config[CONF_POWER_ID])
+
     cg.add(var.set_power_sensor(sens))
     cg.add(var.set_buffer(config[CONF_BUFFER]))
     cg.add(var.set_min_power_demand(config[CONF_MIN_POWER_DEMAND]))
     cg.add(var.set_max_power_demand(config[CONF_MAX_POWER_DEMAND]))
+    cg.add(
+        var.set_power_sensor_inactivity_timeout(
+            config[CONF_POWER_SENSOR_INACTIVITY_TIMEOUT]
+        )
+    )
+    cg.add(var.set_power_demand_calculation(config[CONF_POWER_DEMAND_CALCULATION]))
