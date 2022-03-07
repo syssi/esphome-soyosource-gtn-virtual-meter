2a9537ddeeda35f9f6d3f2c8e26b51d0eb2e997f
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-03-07 09:39:05 +0100

    Add restore_mode option for the manual mode switch

--- components/soyosource_virtual_meter/switch/soyosource_switch.cpp
+++ components/soyosource_virtual_meter/switch/soyosource_switch.cpp
@@ -7,10 +7,57 @@ namespace soyosource_virtual_meter {
 
 static const char *const TAG = "soyosource_virtual_meter.switch";
 
-void SoyosourceSwitch::dump_config() { LOG_SWITCH("", "SoyosourceVirtualMeter Switch", this); }
+void SoyosourceSwitch::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up Soyosource Switch '%s'...", this->name_.c_str());
+
+  bool initial_state = false;
+  switch (this->restore_mode_) {
+    case SOYOSOURCE_SWITCH_RESTORE_DEFAULT_OFF:
+      initial_state = this->get_initial_state().value_or(false);
+      break;
+    case SOYOSOURCE_SWITCH_RESTORE_DEFAULT_ON:
+      initial_state = this->get_initial_state().value_or(true);
+      break;
+    case SOYOSOURCE_SWITCH_ALWAYS_OFF:
+      initial_state = false;
+      break;
+    case SOYOSOURCE_SWITCH_ALWAYS_ON:
+      initial_state = true;
+      break;
+  }
+
+  if (initial_state) {
+    this->turn_on();
+  } else {
+    this->turn_off();
+  }
+}
+void SoyosourceSwitch::dump_config() {
+  LOG_SWITCH("", "SoyosourceVirtualMeter Switch", this);
+  const LogString *restore_mode = LOG_STR("");
+  switch (this->restore_mode_) {
+    case SOYOSOURCE_SWITCH_RESTORE_DEFAULT_OFF:
+      restore_mode = LOG_STR("Restore (Defaults to OFF)");
+      break;
+    case SOYOSOURCE_SWITCH_RESTORE_DEFAULT_ON:
+      restore_mode = LOG_STR("Restore (Defaults to ON)");
+      break;
+    case SOYOSOURCE_SWITCH_ALWAYS_OFF:
+      restore_mode = LOG_STR("Always OFF");
+      break;
+    case SOYOSOURCE_SWITCH_ALWAYS_ON:
+      restore_mode = LOG_STR("Always ON");
+      break;
+  }
+  ESP_LOGCONFIG(TAG, "  Restore Mode: %s", LOG_STR_ARG(restore_mode));
+}
 void SoyosourceSwitch::write_state(bool state) {
   this->parent_->set_manual_mode(state);
   this->publish_state(state);
+}
+
+void SoyosourceSwitch::set_restore_mode(SoyosourceSwitchRestoreMode restore_mode) {
+  this->restore_mode_ = restore_mode;
 }
 
 }  // namespace soyosource_virtual_meter
--- components/soyosource_virtual_meter/switch/soyosource_switch.h
+++ components/soyosource_virtual_meter/switch/soyosource_switch.h
@@ -7,16 +7,25 @@
 namespace esphome {
 namespace soyosource_virtual_meter {
 
+enum SoyosourceSwitchRestoreMode {
+  SOYOSOURCE_SWITCH_RESTORE_DEFAULT_OFF,
+  SOYOSOURCE_SWITCH_RESTORE_DEFAULT_ON,
+  SOYOSOURCE_SWITCH_ALWAYS_OFF,
+  SOYOSOURCE_SWITCH_ALWAYS_ON,
+};
+
 class SoyosourceVirtualMeter;
 
 class SoyosourceSwitch : public switch_::Switch, public Component {
  public:
   void set_parent(SoyosourceVirtualMeter *parent) { this->parent_ = parent; };
+  void set_restore_mode(SoyosourceSwitchRestoreMode restore_mode);
   void dump_config() override;
 
  protected:
   void write_state(bool state) override;
   SoyosourceVirtualMeter *parent_;
+  SoyosourceSwitchRestoreMode restore_mode_{SOYOSOURCE_SWITCH_RESTORE_DEFAULT_OFF};
 };
 
 }  // namespace soyosource_virtual_meter
