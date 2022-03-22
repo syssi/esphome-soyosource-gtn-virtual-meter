2c7a73fb43bf7c00d7e523c09e80bbb5acd78878
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-03-22 08:07:21 +0100

    Add a power demand divider (Closes: #37)

--- components/soyosource_virtual_meter/text_sensor.py
+++ components/soyosource_virtual_meter/text_sensor.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import text_sensor
+import esphome.config_validation as cv
 from esphome.const import CONF_ICON, CONF_ID
 
 from . import CONF_SOYOSOURCE_VIRTUAL_METER_ID, SoyosourceVirtualMeter
