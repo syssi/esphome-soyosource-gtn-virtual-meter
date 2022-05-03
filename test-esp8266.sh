55e38c9ab65a7e2ed63c631b773de8d4e2dae740
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-05-03 09:50:52 +0200

    Remove break statements because of early returns

--- test-esp8266.sh
+++ test-esp8266.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-esphome -s external_components_source components ${1:-run} esp8266-example.yaml
+esphome -s external_components_source components ${1:-run} esp8266-display-example.yaml
+
