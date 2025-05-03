3435bcc1cc18ad5647797aa6ac19ed2d5526f386
Sebastian Muszynski <basti@linkt.de>
2025-05-03 14:31:57 +0200

    Fix lint issue

--- components/soyosource_virtual_meter/switch/soyosource_switch.cpp
+++ components/soyosource_virtual_meter/switch/soyosource_switch.cpp
@@ -34,7 +34,7 @@ void SoyosourceSwitch::setup() {
 }
 void SoyosourceSwitch::dump_config() {
   LOG_SWITCH("", "SoyosourceVirtualMeter Switch", this);
-  const LogString *restore_mode;
+  const LogString *restore_mode = LOG_STR("");
   switch (this->restore_mode_) {
     case SOYOSOURCE_SWITCH_RESTORE_DEFAULT_OFF:
       restore_mode = LOG_STR("Restore (Defaults to OFF)");
