18749864d910561c0c3acd0e9b22c09dc438ede0
Kevin Hübner <k.huebner@ceyoniq.com>
2025-06-02 16:40:19 +0200

    Fix *.*_SCHEMA deprecations for sensor latency

--- components/soyosource_virtual_meter/number/__init__.py
+++ components/soyosource_virtual_meter/number/__init__.py
@@ -172,10 +172,13 @@ CONFIG_SCHEMA = cv.Schema(
             validate,
         ),
         cv.Optional(CONF_MAX_POWER_SENSOR_LATENCY_MS): cv.All(
-            number.NUMBER_SCHEMA.extend(
+            number.number_schema(
+                SoyosourceNumber,
+                icon=ICON_POWER_SENSOR_LATENCY,
+                unit_of_measurement=UNIT_MILLISECOND,
+            )
+            .extend(
                 {
-                    cv.GenerateID(): cv.declare_id(SoyosourceNumber),
-                    cv.Optional(CONF_ICON, default=ICON_POWER_SENSOR_LATENCY): cv.icon,
                     cv.Optional(
                         CONF_MIN_VALUE, default=DEFAULT_MIN_MAX_POWER_SENSOR_LATENCY_MS
                     ): cv.float_,
@@ -184,14 +187,12 @@ CONFIG_SCHEMA = cv.Schema(
                     ): cv.float_,
                     cv.Optional(CONF_STEP, default=DEFAULT_STEP): cv.float_,
                     cv.Optional(
-                        CONF_UNIT_OF_MEASUREMENT, default=UNIT_MILLISECOND
-                    ): cv.string_strict,
-                    cv.Optional(
                         CONF_INITIAL_VALUE, default=DEFAULT_MAX_POWER_SENSOR_LATENCY_MS
                     ): cv.float_,
                     cv.Optional(CONF_RESTORE_VALUE, default=False): cv.boolean,
                 }
-            ).extend(cv.COMPONENT_SCHEMA),
+            )
+            .extend(cv.COMPONENT_SCHEMA),
             validate_min_max,
             validate,
         ),
