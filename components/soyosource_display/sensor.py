702e808f3bede5e2996eb2858fb9454aa2e46dad
Sebastian Muszynski <basti@linkt.de>
2026-03-26 09:24:48 +0100

    Fix bitmask sensors: remove state_class, set device_class=None, add entity_category=diagnostic

--- components/soyosource_display/sensor.py
+++ components/soyosource_display/sensor.py
@@ -70,8 +70,7 @@ CONFIG_SCHEMA = SOYOSOURCE_DISPLAY_COMPONENT_SCHEMA.extend(
             unit_of_measurement=UNIT_EMPTY,
             icon=ICON_ERROR_BITMASK,
             accuracy_decimals=0,
-            device_class=DEVICE_CLASS_EMPTY,
-            state_class=STATE_CLASS_MEASUREMENT,
+            device_class=None,
             entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
         ),
         cv.Optional(CONF_OPERATION_MODE_ID): sensor.sensor_schema(
